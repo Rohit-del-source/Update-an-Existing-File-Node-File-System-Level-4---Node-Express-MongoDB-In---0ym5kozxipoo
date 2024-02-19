b48a76cfff35ca714a5e6707539a1f62fb80052d
Rohit-del-source <rohitverma18158920@gmail.com>
2024-02-19 22:19:19 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
+const datainstant = ", An online learning platform"
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
-  
+  return await fs.appendFile(fileName,fileContent)
 };
+updateFile(fileName,datainstant);
 
 module.exports = updateFile;
